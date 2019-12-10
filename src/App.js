512761789a37f4751fad3135661a731d8a4dc26e
Jordan McCollam <jordy.mccollam@gmail.com>
2019-12-10 15:56:30 -0600

    Little better set up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Pictures from "./cards.json"
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
 import Card from "./components/Card/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <Nav/>
-      <Header/>
-      <div className="container mb-5 pb-5">
-        {/* <div className="row"> */}
-          <Card/>
-        {/* </div> */}
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav/>
+        <Header/>
+        <div className="container mb-5 pb-5">
+          <div className="row text-center">
+          <Card image={Pictures[0].url} />
+          <Card image={Pictures[1].url} />
+          <Card image={Pictures[2].url} />
+          <Card image={Pictures[3].url} />
+          <Card image={Pictures[4].url} />
+          <Card image={Pictures[5].url} />
+          <Card image={Pictures[6].url} />
+          <Card image={Pictures[7].url} />
+          <Card image={Pictures[8].url} />
+          <Card image={Pictures[9].url} />
+          <Card image={Pictures[10].url} />
+          <Card image={Pictures[11].url} />
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,49 +1,13 @@
 import React from "react";
-import Pictures from "./cards.json";
 import "./card.css"
 
-function Card() {
+function Card(props) {
 
-    var randomPicture = Pictures[Math.floor(Math.random() * Pictures.length)].url;
+    // var randomPicture = props[Math.floor(Math.random() * props.length)].url;
 
     return(
-        <div className="row">
-        <div className="col-lg-3">
-            <img src={Pictures[0].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[1].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[2].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[3].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[4].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[5].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[6].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[7].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[8].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[9].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[10].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
-        <div className="col-lg-3">
-            <img src={Pictures[11].url} alt="Clicky Pic"className="clicky-pic" />
-        </div>
+        <div className="col-lg-3 position-relative">
+            <img src={props.image} alt="Clicky Pic"className="clicky-pic" />
         </div>
     );
 }
