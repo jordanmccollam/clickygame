b766dd40dbd1a34b8941f7bd5c2fe5c3d7fd1a0a
Jordan McCollam <jordy.mccollam@gmail.com>
2019-12-11 12:01:49 -0600

    Some styling and responsiveness changes / added instructions

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,19 @@ import HeaderPic from "./header.jpg"
 
 function Header() {
     return(
-        <div>
-            <img src={HeaderPic} alt={"Family Guy Banner"}/>
+        <div className="pt-4">
+            <div className="d-none d-lg-block">
+                <img src={HeaderPic} alt={"Family Guy Banner"} className="position-relative"/>
+                <h3 className="header-text font-primary shadow-text text-light">Click on an image to earn points. 
+                <br/>
+                But don't click on any more than once!</h3>
+            </div>
+            <div className="d-lg-none bg-custom small-header pt-5 d-flex align-items-center justify-content-center">
+                <h3 className="font-primary text-light shadow-text">Click on an image to earn points.
+                    <br/>
+                    But don't click on any more than once!
+                </h3>
+            </div>
         </div>
     );
 }
